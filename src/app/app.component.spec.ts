1cd9cb8139d430a7faac95e0e5ef663eb002faa4
hritikchokker <hchokker7@gmail.com>
2021-05-22 00:58:32 +0530

    new changes pushed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-audioplayer'`, () => {
+  it(`should have as title 'audioplayer-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-audioplayer');
+    expect(app.title).toEqual('audioplayer-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-audioplayer app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('audioplayer-angular app is running!');
   });
 });
